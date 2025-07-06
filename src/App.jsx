76fe367d4544dee37919198af8de01574d6cb6f2
Mahima Rajput <135412470+Mahima34@users.noreply.github.com>
2025-07-06 11:06:19 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 import { Button } from "@/components/ui/button"
+import { Outlet } from 'react-router-dom'
 
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div>
-      <Button className="mt-3 ml-4">Subscribe</Button>
+          <Outlet/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,24 @@ import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import SignInPage from './auth/sign-in/index.jsx'
 import App from './App.jsx'
+import Home from './home/index.jsx'
+import Dashboard from './dashboard/index.jsx'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 const  router=createBrowserRouter([
   {
-    path:'/',
-    element: <App/>
+    element: <App/>,
+    children:[{
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/dashboard',
+      element: <Dashboard />
+    }
+    ],
   },
+
   {
     path: '/auth/sign-in',
     element: <SignInPage />
